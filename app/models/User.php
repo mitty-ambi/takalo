e5489c56a7a537a70b6cd04cd379b3a7a55ca38b
mitty-ambinintsoa <rado23.andria@gmail.com>
2026-02-09 16:42:55 +0300

    Mise à jour

--- app/models/User.php
+++ app/models/User.php
@@ -1,19 +1,19 @@
 <?php
-class User{
+class User
+{
     public $id_user;
     public $username;
     public $email;
     public $password;
     public $role;
 
-    public function __construct($id_user = null, $username = null, $email = null, $password = null, $role = null) {
+    public function __construct($id_user = null, $username = null, $email = null, $password = null, $role = null)
+    {
         $this->id_user = $id_user;
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
         $this->role = $role;
     }
-
-    public function validat
 }
 ?>
